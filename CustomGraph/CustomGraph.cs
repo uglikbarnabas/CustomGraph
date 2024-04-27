d173b34b1f35a66177a17eecda82bfe9fadbe2dc
Uglik Barnabás <uglik.barnabas09@gmail.com>
2024-04-27 17:41:17 +0200

    Added exception when app doesn't have enogh colors to display the graph

--- CustomGraph/CustomGraph.cs
+++ CustomGraph/CustomGraph.cs
@@ -10,12 +10,13 @@ public partial class CustomGraph : Control
 {
     private const int axis_x_height = 50, legend_width = 150;
 
+    private float MaxSection => ChartData.Max(x => x.Data.Count);
+    private float MaxValue => ChartData.Max(x => x.Data.Sum());
+    private float YStep => MaxValue / Yrows;
+
     private int r_legend_width = legend_width;
     private bool r_draw_legend = true;
     private int r_axis_y_width = 50;
-
-    private float MaxValue => ChartData.Max(x => x.Data.Sum());
-    private float YStep => MaxValue / Yrows;
 
     public int Yrows = 6;
 
@@ -50,6 +51,12 @@ public partial class CustomGraph : Control
     protected override void OnPaint(PaintEventArgs e)
     {
         base.OnPaint(e);
+
+        if (ChartColors.Count < MaxSection)
+            throw new IndexOutOfRangeException("Not enough chart colors to cover rendering.\nYou need " +
+                MaxSection + " colors, you currently have " + ChartColors.Count);
+        
+
         r_draw_legend = ClientSize.Width <= 500;
         r_legend_width = r_draw_legend ? 0 : legend_width;
         r_axis_y_width = (int)e.Graphics.MeasureString(MaxValue.ToString(), Font).Width + 10;
@@ -60,8 +67,8 @@ public partial class CustomGraph : Control
         for (int i = 1; i <= Yrows; i++)
         {
             int y = i * (ClientSize.Height - axis_x_height) / Yrows;
-            e.Graphics.DrawLine(Pens.Black, r_axis_y_width - 5, y, r_axis_y_width, y);
             e.Graphics.DrawLine((i == Yrows) ? Pens.Black : Pens.Gray, r_axis_y_width, y, ClientSize.Width - r_legend_width, y);
+            e.Graphics.DrawLine(Pens.Black, r_axis_y_width - 5, y, r_axis_y_width, y);
             int textheight = (int)e.Graphics.MeasureString((MaxValue - YStep * i).ToString(), Font).Height / 2;
             e.Graphics.DrawString(Math.Round((MaxValue - YStep * i)).ToString(), Font, Brushes.Black, 2.5f, y - textheight);
         }
@@ -70,8 +77,8 @@ public partial class CustomGraph : Control
         for (int i = 1; i <= ChartData.Count; i++)
         {
             int x = i * (ClientSize.Width - r_axis_y_width - r_legend_width) / (ChartData.Count + 1) + r_axis_y_width;
-            e.Graphics.DrawLine(Pens.Black, x, ClientSize.Height - axis_x_height, x, ClientSize.Height - axis_x_height + 5);
             e.Graphics.DrawLine(Pens.Gray, x, ClientSize.Height - axis_x_height, x, 0);
+            e.Graphics.DrawLine(Pens.Black, x, ClientSize.Height - axis_x_height, x, ClientSize.Height - axis_x_height + 5);
             int textWidth = (int)e.Graphics.MeasureString(ChartData[i - 1].Name, Font).Width / 2;
             e.Graphics.DrawString(ChartData[i - 1].Name, Font, Brushes.Black, x - textWidth, ClientSize.Height - axis_x_height + 10);
         }
@@ -88,6 +95,13 @@ public partial class CustomGraph : Control
                     availibleHeight - height - YPosMod, (x - r_axis_y_width) / i / 2, height);
                 YPosMod += height;
             }
+        }
+        // Draw legend
+        if (!r_draw_legend) return;
+        float legendStartX = ClientSize.Width - legend_width;
+        for (int i = 0; i < MaxSection; i++)
+        {
+
         }
     }
 }
