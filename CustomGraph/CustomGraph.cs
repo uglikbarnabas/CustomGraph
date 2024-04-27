2e596ceaa24615a09cd10ccd098f306637dd4e4d
Uglik Barnabás <uglik.barnabas09@gmail.com>
2024-04-27 17:28:51 +0200

    Fixed columns not stacking

--- CustomGraph/CustomGraph.cs
+++ CustomGraph/CustomGraph.cs
@@ -8,25 +8,37 @@ namespace CustomGraph;
 
 public partial class CustomGraph : Control
 {
-    private const int axis_x_height = 50, legend_width = 0;
+    private const int axis_x_height = 50, legend_width = 150;
 
     private int r_legend_width = legend_width;
+    private bool r_draw_legend = true;
     private int r_axis_y_width = 50;
 
     private float MaxValue => ChartData.Max(x => x.Data.Sum());
     private float YStep => MaxValue / Yrows;
 
-    public int Yrows = 7;
+    public int Yrows = 6;
 
-    private readonly List<Column> ChartData =
+    private readonly List<ChartColumn> ChartData =
     [
-        new Column() { Data = [ 10, 20 ], Name = "col0" },
-        new Column() { Data = [ 05 ], Name = "col1" },
-        new Column() { Data = [ 10 ], Name = "col2" },
-        new Column() { Data = [ 7.5f ], Name = "col3" },
-        new Column() { Data = [ 00 ], Name = "col4" },
-        new Column() { Data = [ 00 ], Name = "col5" },
-        new Column() { Data = [ 00 ], Name = "col6" },
+        new ChartColumn([ 10, 20 ], "col0"),
+        new ChartColumn([ 05 ], "col1"),
+        new ChartColumn([ 10 ], "col2"),
+        new ChartColumn([ 7.5f ], "col3"),
+        new ChartColumn([ 00 ]),
+        new ChartColumn([ 00 ]),
+        new ChartColumn([ 00 ]),
+    ];
+
+    private readonly List<ChartColorData> ChartColors =
+    [
+        new ChartColorData(new SolidBrush(Color.Red), ""),
+        new ChartColorData(new SolidBrush(Color.Coral), ""),
+        new ChartColorData(new SolidBrush(Color.Blue), ""),
+        new ChartColorData(new SolidBrush(Color.Black), ""),
+        new ChartColorData(new SolidBrush(Color.Brown), ""),
+        new ChartColorData(new SolidBrush(Color.Firebrick), ""),
+        new ChartColorData(new SolidBrush(Color.ForestGreen), ""),
     ];
 
     public CustomGraph()
@@ -38,7 +50,8 @@ public partial class CustomGraph : Control
     protected override void OnPaint(PaintEventArgs e)
     {
         base.OnPaint(e);
-        r_legend_width = (ClientSize.Width <= 500) ? 0 : legend_width;
+        r_draw_legend = ClientSize.Width <= 500;
+        r_legend_width = r_draw_legend ? 0 : legend_width;
         r_axis_y_width = (int)e.Graphics.MeasureString(MaxValue.ToString(), Font).Width + 10;
         e.Graphics.Clear(Color.White);
 
@@ -63,27 +76,21 @@ public partial class CustomGraph : Control
             e.Graphics.DrawString(ChartData[i - 1].Name, Font, Brushes.Black, x - textWidth, ClientSize.Height - axis_x_height + 10);
         }
         // Draw rectangles
-        float totalWidthForRectangles = ClientSize.Width - r_axis_y_width - r_legend_width;
-        float effectiveRectangleWidth = totalWidthForRectangles / ChartData.Count;
-        float rectangleWidth = (float)(effectiveRectangleWidth * 0.5);
         for (int i = 1; i <= ChartData.Count; i++)
         {
             float x = i * (ClientSize.Width - r_axis_y_width - r_legend_width) / (ChartData.Count + 1) + r_axis_y_width;
-            float availibleHeight = ClientSize.Height - axis_x_height; float endY = availibleHeight;
+            float availibleHeight = ClientSize.Height - axis_x_height; float YPosMod = 0;
             float oneUnit = availibleHeight / MaxValue;
-
             for (int j = 0; j < ChartData[i - 1].Data.Count; j++)
             {
                 float height = oneUnit * ChartData[i - 1].Data[j];
-                e.Graphics.FillRectangle(Brushes.Red, x - (x - r_axis_y_width) / i / 4, availibleHeight - height, (x - r_axis_y_width) / i / 2, height);
+                e.Graphics.FillRectangle(ChartColors[j].Color, x - (x - r_axis_y_width) / i / 4,
+                    availibleHeight - height - YPosMod, (x - r_axis_y_width) / i / 2, height);
+                YPosMod += height;
             }
         }
     }
 }
 
-public class Column
-{
-    public List<float> Data = [];
-    public Color Color;
-    public string? Name;
-}
+public record ChartColumn(List<float> Data, string Name = "Unknown");
+public record ChartColorData(Brush Color, string Name = "Unknown");
